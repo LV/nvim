a2afad79200547ecc79f45a5d074148666013110
Luis Victoria <v@lambda.lv>
2025-04-25 01:37:11 -0400

    Add command for new tab

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -99,6 +99,7 @@ return {
 
       { "<leader>w", group = "window" },
       { "<leader>ws", "<cmd>split<CR>", desc = "Split Window Horizontally" },
+      { "<leader>wt", "<cmd>tabnew<CR>", desc = "New Tab" },
       { "<leader>wv", "<cmd>vsplit<CR>", desc = "Split Window Vertically" },
       -- More window navigation keybindings in =config/keymaps.lua=
     },
