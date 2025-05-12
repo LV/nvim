404b7c2c031702f683198c7056f3d0f82a2a8bc5
Luis Victoria <v@lambda.lv>
2025-05-12 16:27:35 -0400

    Remove `input-prompt.lua` (`PromptInput` function)

--- lua/util/input-prompt.lua
+++
@@ -1,9 +0,0 @@
----@param promptDescription string
----@return string
-function PromptInput(promptDescription)
-  local input_text
-    Snacks.input.input({ prompt = string.format("%s", promptDescription) }, function(input)
-    input_text = input
-  end)
-  return input_text
-end
