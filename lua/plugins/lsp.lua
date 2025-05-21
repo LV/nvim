8bd214625ddfe4658630c0d6c04498ef5b661262
Luis Victoria <v@lambda.lv>
2025-05-21 15:41:16 -0400

    Use new mason repo endpoints

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,76 +8,16 @@ local useMason = true
 if useMason then
   return {
     {
-      "williamboman/mason.nvim",
-      lazy = false,
-      cmd = {"Mason", "MasonInstall", "MasonUninstall", "MasonLog"},
-      config = function()
-        require("mason").setup({
-          log_level = vim.log.levels.DEBUG, -- 'DEBUG' (less verbose) or 'TRACE' (more verbose)
-          ui = {
-            icons = {
-              package_installed = "✓",
-              package_pending = "➜",
-              package_uninstalled = "✗"
-            },
-          },
-        })
-      end
+      "mason-org/mason.nvim",
+      opts = {},
     },
     {
-      "williamboman/mason-lspconfig.nvim",
-      lazy = false,
+      "mason-org/mason-lspconfig.nvim",
+      opts = {},
       dependencies = {
-        "williamboman/mason.nvim",
+        { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",
       },
-      config = function()
-        require("mason-lspconfig").setup({
-          -- Specify servers you want automatically installed and set up
-          ensure_installed = {
-            -- Add other servers here that you want automatically installed
-            -- "lua_ls",
-            -- "pyright",
-            -- "rust_analyzer",
-            -- "tsserver",
-          },
-          -- Automatically install servers configured via lspconfig
-          automatic_installation = true,
-        })
-
-        -- This is the crucial part for automatic server setup
-        require("mason-lspconfig").setup_handlers({
-          -- Default handler for installed servers
-          function(server_name)
-            require("lspconfig")[server_name].setup({
-              -- You can specify server-specific settings here if needed
-              -- For example:
-              -- settings = { ... }
-            })
-          end,
-
-          -- You can also set up server-specific handlers
-          -- For example, to add special settings for lua_ls:
-          -- ["lua_ls"] = function()
-          --   require("lspconfig").lua_ls.setup({
-          --     settings = {
-          --       Lua = {
-          --         diagnostics = {
-          --           globals = { "vim" }
-          --         },
-          --         workspace = {
-          --           library = vim.api.nvim_get_runtime_file("", true),
-          --           checkThirdParty = false,
-          --         },
-          --         telemetry = {
-          --           enable = false,
-          --         },
-          --       }
-          --     }
-          --   })
-          -- end,
-          -- Add more server-specific handlers as needed
-        })
-      end
     },
     {
       "neovim/nvim-lspconfig",
