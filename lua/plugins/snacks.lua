29a703606bf95a046c24e88d254554a8d9770026
Luis Victoria <v@luis.vi>
2025-06-04 17:25:28 -0400

    Add `TODO` for making `nvim` snappier with large files

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,7 +6,7 @@ return {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
-    bigfile = { enabled = true },
+    bigfile = { enabled = true },  -- TODO: Disable some more stuff to make it faster
     image = { enabled = true },
     input = { enabled = true },
     notifier = { enabled = true },
