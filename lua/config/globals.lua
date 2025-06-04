f99806d8b74b99f7ea06dfcea9f909c64ef2cf14
Luis Victoria <v@luis.vi>
2025-06-04 16:22:23 -0400

    Add important comment on loading `mapleader` and `maplocalleader` before `lazy.nvim`
    And change single quote to double quotes

--- lua/config/globals.lua
+++ lua/config/globals.lua
@@ -1,5 +1,6 @@
+-- `mapleader` and `maplocalleader` must be setup before `lazy.nvim` loads
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 vim.o.conceallevel=1
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"
