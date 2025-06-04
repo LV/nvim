1e78d0a7800a278b793c874bbbd9423c6f2bde3b
Luis Victoria <v@luis.vi>
2025-06-04 15:48:07 -0400

    Fix bug where headerlines where set globally

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -6,29 +6,19 @@ local config = function()
   })
 
   -- Only apply Org-specific highlight overrides for Bluloco
+  -- https://nvim-orgmode.github.io/configuration#colors
   vim.api.nvim_create_autocmd("FileType", {
     pattern = "org",
     callback = function()
       if vim.g.colors_name == "bluloco-dark" or vim.g.colors_name == "bluloco" then
-        local bluloco_colors = {
-          title      = "#10b1fe",  -- Blue
-          constant   = "#ff78f8",  -- Magenta
-          identifier = "#3fc56b",  -- Green
-          statement  = "#58c6ff",  -- Light blue-ish
-          preproc    = "#ff936a",  -- Orange
-          type       = "#f9c859",  -- Yellow
-          special    = "#9f7efe",  -- Violet
-          string     = "#ce9887",  -- Brown
-        }
-
-        vim.api.nvim_set_hl(0, 'Title',     { fg = bluloco_colors.title,     bold = true })
-        vim.api.nvim_set_hl(0, 'Constant',  { fg = bluloco_colors.constant,  bold = true })
-        vim.api.nvim_set_hl(0, 'Identifier',{ fg = bluloco_colors.identifier,bold = true })
-        vim.api.nvim_set_hl(0, 'Statement', { fg = bluloco_colors.statement, bold = true })
-        vim.api.nvim_set_hl(0, 'PreProc',   { fg = bluloco_colors.preproc,   bold = true })
-        vim.api.nvim_set_hl(0, 'Type',      { fg = bluloco_colors.type,      bold = true })
-        vim.api.nvim_set_hl(0, 'Special',   { fg = bluloco_colors.special,   bold = true })
-        vim.api.nvim_set_hl(0, 'String',    { fg = bluloco_colors.string,    bold = true })
+        vim.api.nvim_set_hl(0, "@org.headline.level1", { fg = "#10b1fe", bold = true })  -- Blue
+        vim.api.nvim_set_hl(0, "@org.headline.level2", { fg = "#ff78f8", bold = true })  -- Magenta
+        vim.api.nvim_set_hl(0, "@org.headline.level3", { fg = "#3fc56b", bold = true })  -- Green
+        vim.api.nvim_set_hl(0, "@org.headline.level4", { fg = "#58c6ff", bold = true })  -- Light Blue
+        vim.api.nvim_set_hl(0, "@org.headline.level5", { fg = "#ff936a", bold = true })  -- Orange
+        vim.api.nvim_set_hl(0, "@org.headline.level6", { fg = "#f9c859", bold = true })  -- Yellow
+        vim.api.nvim_set_hl(0, "@org.headline.level7", { fg = "#9f7efe", bold = true })  -- Violet
+        vim.api.nvim_set_hl(0, "@org.headline.level8", { fg = "#ce9887", bold = true })  -- Brown
       end
     end,
   })
