3a841a7e9dc532a8fcdf5dfa0dd34860ad790601
Luis Victoria <v@luis.vi>
2025-06-04 17:31:36 -0400

    Fix bad window navigation command

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,10 +2,10 @@ local mapkey = require("util.keymapper").mapvimkey
 -- mapvimkey(keymaps, command, vimmode, options)
 
 -- Pane and Window Navigation
-mapkey("<leader>wk", "<C-w>k", "n", { desc = "Navigate Up" })
-mapkey("<leader>wj", "<C-w>j", "n", { desc = "Navigate Down" })
-mapkey("<leader>wh", "<C-w>h", "n", { desc = "Navigate Left" })
-mapkey("<leader>wl", "<C-w>l", "n", { desc = "Navigate Right" })
+mapkey("<leader>wk", "wincmd k", "n", { desc = "Navigate Up" })
+mapkey("<leader>wj", "wincmd j", "n", { desc = "Navigate Down" })
+mapkey("<leader>wh", "wincmd h", "n", { desc = "Navigate Left" })
+mapkey("<leader>wl", "wincmd l", "n", { desc = "Navigate Right" })
 
 
 -- Go to normal mode from terminal mode by pressing Escape
