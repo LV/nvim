f3bb3eb533a044889c95a3923f1c09ee70f3a90a
Luis Victoria <v@luis.vi>
2025-06-04 16:33:23 -0400

    Stop `orgmode.nvim` from automatically indenting text underneath headers

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -1,5 +1,7 @@
+-- https://nvim-orgmode.github.io/configuration
 local config = function()
   require("orgmode").setup({
+    org_adapt_indentation = false,
     org_agenda_files = "~/v/agenda",
     org_default_notes_file = "~/v/refile.org",
     org_startup_folded = "showeverything",
