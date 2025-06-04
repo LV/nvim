f45936ccca6e3e53b828f089a52973bb7999376c
Luis Victoria <v@luis.vi>
2025-06-04 15:09:07 -0400

    Launch `.org` file while showing everything (instead of folded)

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -1,12 +1,14 @@
+local config = function()
+  require("orgmode").setup({
+    org_agenda_files = "~/v/agenda",
+    org_default_notes_file = "~/v/refile.org",
+    org_startup_folded = "showeverything",
+  })
+end
+
 return {
   "nvim-orgmode/orgmode",
   event = "VeryLazy",
   ft = { "org" },
-  config = function()
-    -- Setup orgmode
-    require("orgmode").setup({
-      org_agenda_files = "~/v/agenda",
-      org_default_notes_file = "~/v/refile.org",
-    })
-  end,
+  config = config,
 }
