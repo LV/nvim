ceb38a16d5e86bd1640270c173e2c9b5871cde72
Luis Victoria <v@luis.vi>
2025-06-05 10:13:02 -0400

    Add file docstring and better comments to `lua/config/globals.lua`

--- lua/config/globals.lua
+++ lua/config/globals.lua
@@ -1,6 +1,10 @@
--- `mapleader` and `maplocalleader` must be setup before `lazy.nvim` loads
+-- lua/config/globals.lua
+-- Global variables and early runtime options
+
+-- Leader keys (must be set before `Lazy.nvim` loads)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+-- Global editor options
 vim.o.conceallevel=1
 vim.opt.clipboard = "unnamedplus"
