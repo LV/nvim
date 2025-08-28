ac2c99d1881b3bef62b4007846a29ad630f3b85e
Luis Victoria <v@luis.vi>
2025-08-27 22:21:06 -0400

    Show characters when toggling `indent-blankline`

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -3,7 +3,22 @@ return {
   main = "ibl",
   ---@module "ibl"
   ---@type ibl.config
-  opts = { enabled = false },  -- press keybinding once to enable instead of twice (due to toggle)
-  keys = { { "<leader>mi", "<cmd>IBLToggle<CR>", desc = "Toggle Indentation Guide" } },
+  opts = {
+    enabled = false,  -- press keybinding once to enable instead of twice (due to toggle)
+    scope = { enabled = false },
+  },
+  keys = {
+    { "<leader>mi", function()
+      -- Toggle IBL
+      vim.cmd("IBLToggle")
+      -- Also toggle whitespace display with periods
+      if vim.opt.list:get() then
+        vim.opt.list = false
+      else
+        vim.opt.list = true
+        vim.opt.listchars = { tab = "→ ", space = "·", trail = "~", extends = ">", precedes = "<" }
+      end
+    end, desc = "Toggle Indentation Guide & Whitespace" }
+  },
   lazy = true,
 }
