502402c10f5562b1be7b76604acc2b704b98ef30
Luis Victoria <v@luis.vi>
2025-09-21 12:03:34 -0400

    Use SSH only for Lazy

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,48 +1,43 @@
--- lua/config/init.lua
--- Neovim core configuration loader and `Lazy.nvim` bootstrapper
-
-
--- Bootstrap `lazy.nvim`
----@type string
+-- bootstrap lazy.nvim (put this at the VERY top of init.lua)
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  -- choose ONE of these two remotes, depending on what works on your network:
+  -- local repo = "https://github.com/folke/lazy.nvim.git"  -- use this if HTTPS works
+  local repo = "git@github.com:folke/lazy.nvim.git"  -- use this if ONLY SSH works
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", repo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
-
 vim.opt.rtp:prepend(lazypath)
 
-
--- Load core configurations before `Lazy.nvim`
+-- Load core configs (must not require plugins yet)
 require("config.globals")
 require("config.indentation")
 require("config.options")
 
-
--- `Lazy.nvim` setup
--- https://lazy.folke.io/configuration
--- TODO: Review whether to change `defaults` to `false` as suggested in docs
--- TODO: Review whether to remove `rtp`
--- TODO: Review whether to change notification for `change_detection`
--- TODO: Add type notation to `opts`
+-- `Lazy.nvim` setup (force SSH for all plugin clones)
 local opts = {
   defaults = { lazy = true },
   rtp = {
     disabled_plugins = {
-      "gzip",
-      "matchit",
-      "matchparen",
-      "netrw",
-      "netrwPlugin",
-      "tarPlugin",
-      "tohtml",
-      "tutor",
-      "zipPlugin",
-    }
+      "gzip","matchit","matchparen","netrw","netrwPlugin",
+      "tarPlugin","tohtml","tutor","zipPlugin",
+    },
   },
   spec = { import = "plugins" },
   change_detection = { notify = false },
-}
 
+  -- 👇 this makes every plugin use SSH over 443
+  git = {
+    url_format = "git@github.com:%s.git",
+    timeout = 300,
+  },
+}
 require("lazy").setup(opts)
