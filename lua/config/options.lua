1c11010d59738b772f8ca0037a16c6320c672a11
Luis Victoria <v@luis.vi>
2025-10-07 01:19:02 -0400

    Remove trailing whitespace on save

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -14,3 +14,13 @@ vim.opt.termguicolors = true
 
 -- Behavior
 vim.opt.encoding = "UTF-8"
+
+-- Remove trailing whitespace on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    local save_cursor = vim.fn.getpos(".")
+    vim.cmd([[%s/\s\+$//e]])
+    vim.fn.setpos(".", save_cursor)
+  end,
+})
