a4713749ea40aa191c39f6e1bd4b7c4ab1d8706d
Luis Victoria <v@luis.vi>
2026-01-13 15:07:16 -0500

    chore(lua-ls): load lazy types for `LazySpec...`

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -8,6 +8,8 @@ return {
         -- See the configuration section for more details
         -- Load luvit types when the `vim.uv` word is found
         { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        -- Load lazy.nvim types for LazySpec, LazyPluginSpec, etc
+        { path = "lazy.nvim", words = { "Lazy%w+" } },
       },
     },
   },
