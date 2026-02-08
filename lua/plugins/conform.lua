c0561fa875d9f778ebe646242a033e45803b8269
Luis Victoria <v@luis.vi>
2026-02-07 21:43:43 -0500

    feat(plugins/conform): automatically format python files

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,14 @@
+---@type LazyPluginSpec
+return {
+  "stevearc/conform.nvim",
+  event = "BufWritePre",
+  opts = {
+    formatters_by_ft = {
+      python = { "ruff_format" },
+    },
+    format_on_save = {
+      timeout_ms = 1000,
+      lsp_format = "never",
+    },
+  },
+}
